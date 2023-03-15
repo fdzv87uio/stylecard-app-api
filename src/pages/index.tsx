0ba17244aef7da08999c4d6b701e8965d2b8beff
Fausto Zambrano <fausto@faqx.com>
2023-03-15 09:41:52 -0500

    erasing imports for next-auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
-import { useSession, signIn } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession();
   return (
     <>
       <p>Stylecard Web App / API</p>
