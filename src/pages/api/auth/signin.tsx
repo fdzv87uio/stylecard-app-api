0f64f6df43300ef1035c85ec6d82475c304ffc04
Fausto Zambrano <fausto@faqx.com>
2023-03-15 08:27:53 -0500

    adding cors to all endpoints

--- src/pages/api/auth/signin.tsx
+++ src/pages/api/auth/signin.tsx
@@ -2,26 +2,41 @@ import Session from "@/models/Session";
 import User from "@/models/User";
 import { connectMongoose } from "@/utils/connectMongoose";
 import { compare, hash } from "bcryptjs";
-import mongoose from "mongoose";
 import type { NextApiRequest, NextApiResponse } from "next";
-import NextCors from "nextjs-cors";
+import Cors from "cors";
+
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
   await connectMongoose().catch((error) => res.json(error));
   const { email, password } = req.body;
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
   if (req.method !== "POST")
     res.status(409).json({ error: "Html Method not allowed" });
   if (!email) res.status(409).json({ error: "Email was not provided" });
