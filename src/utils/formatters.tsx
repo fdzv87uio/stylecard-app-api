e61acec9dacf71d78eb3f6ccbb916c210777d2c4
Fausto Zambrano <fausto@faqx.com>
2023-03-29 09:58:16 -0500

    changing token formatting

--- src/utils/formatters.tsx
+++ src/utils/formatters.tsx
@@ -1,11 +1,11 @@
 export function getUrlSafeString(item: string) {
-  let res = item.replace("/", "slash");
-  res.replace("+", "*");
+  let res = item.replaceAll("/", "slash");
+  res.replaceAll("+", "*");
   return res;
 }
 
 export function getUrlUnsafeString(item: string) {
-  let res = item.replace("slash", "/");
-  res.replace("*", "+");
+  let res = item.replaceAll("slash", "/");
+  res.replaceAll("*", "+");
   return res;
 }
