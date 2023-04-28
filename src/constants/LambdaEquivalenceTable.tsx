7efb2883047d51d22f4aa3ba7caf9b00bd101e86
Fausto Zambrano <fausto@faqx.com>
2023-04-28 16:32:13 -0500

    adjunsting equivalence table

--- src/constants/LambdaEquivalenceTable.tsx
+++ src/constants/LambdaEquivalenceTable.tsx
@@ -1,6 +1,6 @@
 export const LambdaEquivalenceTable = [
-    { number: 1, ratio: 0.4 },
-    { number: 2, ratio: 0.45 },
+    { number: 1, ratio: 0.1 },
+    { number: 2, ratio: 0.25 },
     { number: 3, ratio: 0.5 },
     { number: 4, ratio: 0.7 },
     { number: 5, ratio: 0.75 },
