0f25b0fff9a78c33be4632e2dd25762c6952388a
Fausto Zambrano <fausto@faqx.com>
2023-05-12 10:32:03 -0500

    changing equvalence intervals for beta

--- src/constants/LambdaEquivalenceTable.tsx
+++ src/constants/LambdaEquivalenceTable.tsx
@@ -1,8 +1,8 @@
 export const LambdaEquivalenceTable = [
-    { number: 1, ratio: 0.8 },
-    { number: 2, ratio: 0.9 },
+    { number: 1, ratio: 0.9 },
+    { number: 2, ratio: 0.95 },
     { number: 3, ratio: 1 },
-    { number: 4, ratio: 1.10 },
-    { number: 5, ratio: 1.20 },
+    { number: 4, ratio: 1.05 },
+    { number: 5, ratio: 1.10 },
 
 ]
