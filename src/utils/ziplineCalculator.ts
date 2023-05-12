71babd3160cbdbbcc3f28dde65033dec26782d3f
Fausto Zambrano <fausto@faqx.com>
2023-05-12 11:13:34 -0500

    adding new positive values for ranking Equivalence table

--- src/utils/ziplineCalculator.ts
+++ src/utils/ziplineCalculator.ts
@@ -88,6 +88,10 @@ function getRankingFromScalar(value: number, unit: string) {
         return 1;
     } else if (currentValue === 0) {
         return 1;
+    } else if (currentValue > 0 && currentValue < 0.5) {
+        return 0.75;
+    } else if (currentValue >= 0.5 && currentValue <= 1) {
+        return 0.5;
     } else {
         return 0;
     }
