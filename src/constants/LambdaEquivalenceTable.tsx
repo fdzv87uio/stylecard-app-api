c2bd79c0c4f771c0eeaef9e8436a65b6c8fedc95
Fausto Zambrano <fausto@faqx.com>
2023-05-12 11:19:29 -0500

    re adjusting intervals

--- src/constants/LambdaEquivalenceTable.tsx
+++ src/constants/LambdaEquivalenceTable.tsx
@@ -1,8 +1,8 @@
 export const LambdaEquivalenceTable = [
-    { number: 1, ratio: 0.9 },
-    { number: 2, ratio: 0.95 },
+    { number: 1, ratio: 0.95 },
+    { number: 2, ratio: 0.975 },
     { number: 3, ratio: 1 },
-    { number: 4, ratio: 1.05 },
-    { number: 5, ratio: 1.10 },
+    { number: 4, ratio: 1.025 },
+    { number: 5, ratio: 1.05 },
 
 ]
