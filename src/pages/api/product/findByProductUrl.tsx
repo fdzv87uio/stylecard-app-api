b6793af165ebfc0a6a13092b3393144db3583fb3
Fausto Zambrano <fausto@faqx.com>
2023-06-10 11:59:54 -0500

    uncommenting fixes

--- src/pages/api/product/findByProductUrl.tsx
+++ src/pages/api/product/findByProductUrl.tsx
@@ -19,7 +19,7 @@ export default async function handler(
     try {
         console.log("Fetching Product");
         const filterProduct = { product_url: product_url };
-        const productResponse: any = await Product.findOne(filterProduct);
+        const productResponse: any = await Product.find(filterProduct);
         console.log(productResponse);
         console.log("Fetching User");
         const filterUser = { _id: user_id };
@@ -28,50 +28,50 @@ export default async function handler(
         console.log("Fetching user Style Preferences ");
         const userStylePreferences = await StylePreference.findOne({ creator_id: user_id });
         console.log("SP Fetched");
-        const userMeasurements = {
-            chest: userResponse.chest,
-            waist: userResponse.waist,
-            hip: userResponse.hips,
-            unit: userResponse.units,
-        };
-        const currentZiplineResult = getZiplineRanking(userMeasurements, userStylePreferences, productResponse);
-        let resultObject = { item: productResponse, ranking: currentZiplineResult };
-        return res.status(200).json({ status: "success", data: resultObject });
-        // if (productResponse.length === 1) {
-        //     const userMeasurements = {
-        //         chest: userResponse.chest,
-        //         waist: userResponse.waist,
-        //         hip: userResponse.hips,
-        //         unit: userResponse.units,
-        //     };
-        //     const currentZiplineResult = getZiplineRanking(userMeasurements, userStylePreferences, productResponse[0]);
-        //     let resultObject = { item: productResponse, ranking: currentZiplineResult };
-        //     return res.status(200).json({ status: "success", data: resultObject });
-        // } else if (productResponse.length > 1) {
-        //     let resultArray: any = [];
-        //     productResponse.forEach((item: any) => {
-        //         const userMeasurements = {
-        //             chest: userResponse.chest,
-        //             waist: userResponse.waist,
-        //             hip: userResponse.hips,
-        //             unit: userResponse.units,
-        //         };
-        //         const currentZiplineResult = getZiplineRanking(userMeasurements, userStylePreferences, item);
-        //         let resultObject = { item: productResponse, ranking: currentZiplineResult };
-        //         resultArray.push(resultObject);
-        //     })
-        //     resultArray.sort(function (a: any, b: any) {
-        //         if (a.ranking.ranking_avg > b.ranking.ranking_avg) {
-        //             return -1;
-        //         }
-        //         if (a.ranking.ranking_avg < b.ranking.ranking_avg) {
-        //             return 1;
-        //         }
-        //         // names must be equal
-        //         return 0;
-        //     });
-        //     return res.status(200).json({ status: "success", data: resultArray[0] });
-        // }
+        // const userMeasurements = {
+        //     chest: userResponse.chest,
+        //     waist: userResponse.waist,
+        //     hip: userResponse.hips,
+        //     unit: userResponse.units,
+        // };
+        // const currentZiplineResult = getZiplineRanking(userMeasurements, userStylePreferences, productResponse);
+        // let resultObject = { item: productResponse, ranking: currentZiplineResult };
+        // return res.status(200).json({ status: "success", data: resultObject });
+        if (productResponse.length === 1) {
+            const userMeasurements = {
+                chest: userResponse.chest,
+                waist: userResponse.waist,
+                hip: userResponse.hips,
+                unit: userResponse.units,
+            };
+            const currentZiplineResult = getZiplineRanking(userMeasurements, userStylePreferences, productResponse[0]);
+            let resultObject = { item: productResponse, ranking: currentZiplineResult };
+            return res.status(200).json({ status: "success", data: resultObject });
+        } else if (productResponse.length > 1) {
+            let resultArray: any = [];
+            productResponse.forEach((item: any) => {
+                const userMeasurements = {
+                    chest: userResponse.chest,
+                    waist: userResponse.waist,
+                    hip: userResponse.hips,
+                    unit: userResponse.units,
+                };
+                const currentZiplineResult = getZiplineRanking(userMeasurements, userStylePreferences, item);
+                let resultObject = { item: productResponse, ranking: currentZiplineResult };
+                resultArray.push(resultObject);
+            })
+            resultArray.sort(function (a: any, b: any) {
+                if (a.ranking.ranking_avg > b.ranking.ranking_avg) {
+                    return -1;
+                }
+                if (a.ranking.ranking_avg < b.ranking.ranking_avg) {
+                    return 1;
+                }
+                // names must be equal
+                return 0;
+            });
+            return res.status(200).json({ status: "success", data: resultArray[0] });
+        }
     } catch (error) {
         res.status(409).json({
             error: "An error occurred while fetching product: " + error,
