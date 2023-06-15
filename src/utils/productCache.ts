e665a6a206ea62147c2c87b57411c73b3f53f4ec
Fausto Zambrano <fausto@faqx.com>
2023-06-14 20:35:32 -0500

    fixing samples to 100

--- src/utils/productCache.ts
+++ src/utils/productCache.ts
@@ -9,12 +9,12 @@ export async function getAllCachedProducts(gender: string) {
         console.log("using cached DB Products");
         return cachedProducts; // Prefer cached connection
     }
-    const productList1 = await Product.aggregate([{ $match: { $and: [{ brand_name: { $eq: "Gant" } }, { gender: { $eq: gender } }] } }, { $sample: { size: 200 } }]);
-    const productList2 = await Product.aggregate([{ $match: { $and: [{ brand_name: { $eq: "Dresslily" } }, { gender: { $eq: gender } }] } }, { $sample: { size: 200 } }]);
-    const productList3 = await Product.aggregate([{ $match: { $and: [{ brand_name: { $eq: "Joules" } }, { gender: { $eq: gender } }] } }, { $sample: { size: 200 } }]);
-    const productList4 = await Product.aggregate([{ $match: { $and: [{ brand_name: { $eq: "Ted Baker" } }, { gender: { $eq: gender } }] } }, { $sample: { size: 200 } }]);
-    const productList5 = await Product.aggregate([{ $match: { $and: [{ brand_name: { $eq: "Hello Molly" } }, { gender: { $eq: gender } }] } }, { $sample: { size: 200 } }]);
-    const productList6 = await Product.aggregate([{ $match: { $and: [{ brand_name: { $eq: "Sweaty Betty" } }, { gender: { $eq: gender } }] } }, { $sample: { size: 200 } }]);
+    const productList1 = await Product.aggregate([{ $match: { $and: [{ brand_name: { $eq: "Gant" } }, { gender: { $eq: gender } }] } }, { $sample: { size: 100 } }]);
+    const productList2 = await Product.aggregate([{ $match: { $and: [{ brand_name: { $eq: "Dresslily" } }, { gender: { $eq: gender } }] } }, { $sample: { size: 100 } }]);
+    const productList3 = await Product.aggregate([{ $match: { $and: [{ brand_name: { $eq: "Joules" } }, { gender: { $eq: gender } }] } }, { $sample: { size: 100 } }]);
+    const productList4 = await Product.aggregate([{ $match: { $and: [{ brand_name: { $eq: "Ted Baker" } }, { gender: { $eq: gender } }] } }, { $sample: { size: 100 } }]);
+    const productList5 = await Product.aggregate([{ $match: { $and: [{ brand_name: { $eq: "Hello Molly" } }, { gender: { $eq: gender } }] } }, { $sample: { size: 100 } }]);
+    const productList6 = await Product.aggregate([{ $match: { $and: [{ brand_name: { $eq: "Sweaty Betty" } }, { gender: { $eq: gender } }] } }, { $sample: { size: 100 } }]);
     const productList = productList1.concat(productList2, productList3, productList4, productList5, productList6);
     console.log("using new DB Products");
     console.log(productList);
